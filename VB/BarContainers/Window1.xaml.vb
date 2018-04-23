b74ae65f3b949686e622d6672c96a3ea7db4356a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/BarContainers/Window1.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Windows
+
+Namespace BarContainers
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Partial Public Class Window1
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
